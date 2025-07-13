aab39f50f920becba6e97411569587511f7bc6f6
yunzhaol <yunzhao.li@mail.utoronto.ca>
2025-07-13 00:43:07 -0400

    finish the unittest

--- src/test/java/use_case/login/LoginInteractorTest.java
+++ src/test/java/use_case/login/LoginInteractorTest.java
@@ -65,6 +65,7 @@ public class LoginInteractorTest {
         };
 
         LoginInputBoundary interactor = new LoginInteractor(userRepository, successPresenter);
+        assertNull(userRepository.getCurrentUser());
         interactor.execute(inputData);
         assertEquals("Paul", userRepository.getCurrentUser());
     }
